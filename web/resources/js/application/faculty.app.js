7ac008ed2b0f734a01e3001a6f56baa67665d346
jmaicaaan <jmsantosss29@yahoo.com>
2016-01-27 13:07:54 +0800

    Initial commit

--- web/resources/js/application/faculty.app.js
+++ web/resources/js/application/faculty.app.js
@@ -67,35 +67,7 @@
 			.state("dashboard.settings", {
 				url: "/settings",
 				templateUrl: TEMP_LOC + "settings.html",
-				controller: function($state){
-					$state.go("dashboard.settings.general");
-				},
-				controllerAs: "settings"
-			})
-			.state("dashboard.settings.general",{
-				url: "/general",
-				templateUrl: TEMP_LOC + "settings/settings.general.html",
-				controller: function($mdDialog){
-					var self = this;
-
-					self.showDialogPassword = showDialogPassword;
-					self.listOfSubjects = ["ISPROG1", "PROGAP1", "IS-EBIZ", "PROGAP2", "WEBDEVT"];
-					self.selectedChips = [];
-					self.user = {
-						"firstname": "Jm",
-						"lastname": "Santos",
-						"username": "santosjm"
-					};
-
-					function showDialogPassword(event){
-						$mdDialog.show({
-							parent: angular.element(document.body),
-							targetEvent: event,
-							templateUrl: TEMP_LOC + "settings/settings.general.password.html",
-							clickOutsideToClose: true
-						   });
-					}
-				},
+				controller: "genSetCtrl",
 				controllerAs: "genSet"
 			});
 	}
---
+++ web/resources/js/controllers/settings/genSetCtrl.js
@@ -0,0 +1,26 @@
+(function(){
+	angular.module("facultyApp")
+		.controller("genSetCtrl", genSetCtrl);
+
+	function genSetCtrl($mdDialog){
+		var self = this;
+
+		self.showDialogPassword = showDialogPassword;
+		self.listOfSubjects = ["ISPROG1", "PROGAP1", "IS-EBIZ", "PROGAP2", "WEBDEVT"];
+		self.selectedChips = [];
+		self.user = {
+			"firstname": "Jm",
+			"lastname": "Santos",
+			"username": "santosjm"
+		};
+
+		function showDialogPassword(event){
+			$mdDialog.show({
+				parent: angular.element(document.body),
+				targetEvent: event,
+				templateUrl: TEMP_LOC + "settings/settings.general.password.html",
+				clickOutsideToClose: true
+			   });
+		}
+	}
+}());
