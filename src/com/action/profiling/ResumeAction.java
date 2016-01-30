523b938781f421c184e28acb4c5cec32a307ccc4
jmaicaaan <jmaicaaan@users.noreply.github.com>
2016-01-30 12:22:52 +0800

    Update ResumeAction.java

--- src/com/action/profiling/ResumeAction.java
+++ src/com/action/profiling/ResumeAction.java
@@ -4,18 +4,14 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.struts2.interceptor.ServletRequestAware;
 
-<<<<<<< 31e3ba1d682ad4144441b0b70d9ab393d78d4f87
 import com.HibernateUtil.ProfilingHelper;
 import com.model.Resume;
-=======
->>>>>>> Resume
 import com.model.ResumeModel;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
 
 public class ResumeAction extends ActionSupport implements ModelDriven<ResumeModel>, ServletRequestAware{
 	
-<<<<<<< 31e3ba1d682ad4144441b0b70d9ab393d78d4f87
 	
 	private static final long serialVersionUID = 1L;
 	private HttpServletRequest request;
@@ -33,7 +29,6 @@ public class ResumeAction extends ActionSupport implements ModelDriven<ResumeMod
 		
 		resume=new Resume(rModel.getResumeUrl());
 		session_Helper.addResume(resume);
-=======
 	/**
 	 * 
 	 */
@@ -48,27 +43,18 @@ public class ResumeAction extends ActionSupport implements ModelDriven<ResumeMod
 		String serverPath = request.getServletContext().getRealPath("/");
 		rModel.doUpload(serverPath);
 		System.out.println(rModel.getResumeUrl());
->>>>>>> Resume
 		return SUCCESS;
 	}
 	
 	@Override
 	public ResumeModel getModel() {
-<<<<<<< 31e3ba1d682ad4144441b0b70d9ab393d78d4f87
-		
-=======
 		// TODO Auto-generated method stub
->>>>>>> Resume
 		return rModel;
 	}
 	
 	@Override
 	public void setServletRequest(HttpServletRequest request) {
-<<<<<<< 31e3ba1d682ad4144441b0b70d9ab393d78d4f87
-		
-=======
 		// TODO Auto-generated method stub
->>>>>>> Resume
 		this.request = request;
 		
 	}
