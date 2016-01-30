996939764f6ee06c3197b23617ba1bb8ffbc7d1f
jmaicaaan <jmaicaaan@users.noreply.github.com>
2016-01-30 12:21:47 +0800

    Update Achievement_Certificate_Action.java

--- src/com/action/profiling/Achievement_Certificate_Action.java
+++ src/com/action/profiling/Achievement_Certificate_Action.java
@@ -3,7 +3,6 @@ package com.action.profiling;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.struts2.interceptor.ServletRequestAware;
-<<<<<<< 2793fc1397d801b36b82b260abd4fc507180dc31
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -11,10 +10,8 @@ import org.hibernate.cfg.Configuration;
 import com.HibernateUtil.ProfilingHelper;
 import com.model.Achievement_Certificate_Model;
 import com.model.Achievements;
-=======
 
 import com.model.Achievement_Certificate_Model;
->>>>>>> missing db
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
 
@@ -31,27 +28,18 @@ public class Achievement_Certificate_Action extends ActionSupport
 	
 	private Achievement_Certificate_Model acModel = new Achievement_Certificate_Model();
 	
-<<<<<<< 2793fc1397d801b36b82b260abd4fc507180dc31
 	private Achievements achievements;
 	ProfilingHelper session_Helper=new ProfilingHelper();
 
-=======
->>>>>>> missing db
-	
 	@Override
 	public String execute() throws Exception {
 		
 		String serverPath = request.getServletContext().getRealPath("/");
 		
 		acModel.doUpload(serverPath);
-<<<<<<< 2793fc1397d801b36b82b260abd4fc507180dc31
 		achievements=new Achievements(acModel.getAchievement_Certificate_Name(), acModel.getAchievement_Certificate_Url());
 		
 		session_Helper.addAchievements(achievements);
-=======
-		
->>>>>>> missing db
-		
 		return SUCCESS;
 	}
 	@Override
