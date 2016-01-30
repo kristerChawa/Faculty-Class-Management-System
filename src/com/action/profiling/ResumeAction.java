a89c6a07ac295430339088263e9874f73856b10d
jmaicaaan <jmsantosss29@yahoo.com>
2016-01-30 12:28:15 +0800

    Removed crlf

--- src/com/action/profiling/ResumeAction.java
+++ src/com/action/profiling/ResumeAction.java
@@ -12,23 +12,6 @@ import com.opensymphony.xwork2.ModelDriven;
 
 public class ResumeAction extends ActionSupport implements ModelDriven<ResumeModel>, ServletRequestAware{
 	
-	
-	private static final long serialVersionUID = 1L;
-	private HttpServletRequest request;
-	private ResumeModel rModel = new ResumeModel();
-	private Resume resume;
-	ProfilingHelper session_Helper=new ProfilingHelper();
-	
-	
-	@Override
-	public String execute() throws Exception 
-	{
-		
-		String serverPath = request.getServletContext().getRealPath("/");
-		rModel.doUpload(serverPath);
-		
-		resume=new Resume(rModel.getResumeUrl());
-		session_Helper.addResume(resume);
 	/**
 	 * 
 	 */
